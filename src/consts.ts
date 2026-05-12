1948b8104dc1f8ee4c3c814e3fed8fa8bf2e36d4
a2005940203-afk <a2005940203@email.com>
2026-05-12 19:50:14 +0800

    Initial commit: Astro news site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '探路者';
+export const SITE_DESCRIPTION = '探索世界的每一個角落';
